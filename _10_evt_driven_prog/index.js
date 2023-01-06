4763def31183f56deb8498153a73c80c357eb543
rajan <rajyotiroshan@gmail.com>
2023-01-06 20:29:01 +0530

    evt driven prog final v1.0

--- _10_evt_driven_prog/index.js
+++ _10_evt_driven_prog/index.js
@@ -1,4 +1,4 @@
-//select some dom ele to lisen evt on tethem
+/* //select some dom ele to lisen evt on tethem
 const btn = document.querySelector("button");
 //btn.addEventListener("click", onClick);
 
@@ -26,7 +26,7 @@ const btn = document.querySelector("button");
 //going back up the DOM
 
 //evt.stopPropagation(); //stop the bubbleing phase -> only
-
+/*
 function onClick(evt) {
   //evt.stopPropagation(); //no body click fired
   //console.log(evt);
@@ -64,7 +64,7 @@ function onClick(evt) {
   //Note: this -> whatever on the addEventListener is called
   // evt.target -> whatever on the evt happen
 }
- */
+ */ /*
 function onClick(evt) {
   console.log("btn clicked");
   console.log(evt.target);
@@ -73,5 +73,84 @@ function onClick(evt) {
 
 btn.addEventListener("click", onClick);
 //btn.removeEventListener("click", onClick);
-
+ */
 //common events.
+
+//mouseclick : evt fired when mouse is clicked and released
+//-> both mousedown and mouseup
+//only left btn on the mouse
+
+//mousedown : fired when mouse down, do not wait for release
+//both buton
+//mouseup: when mouse is released
+
+/* window.addEventListener("keyup", (evt) => {
+  console.log(evt.code);
+}); */
+
+//keypress -> deprecated
+
+//scrollable
+/* 
+const scrollable = document.getElementById("scrollable");
+scrollable.addEventListener("scroll", (evt) => {
+  console.log(evt.target.scrollTop); // how far down scrolled
+});
+
+scrollable.addEventListener("mouseenter", (evt) => {
+  console.log(evt.pageX, evt.pageY); // how far down scrolled
+});
+
+scrollable.addEventListener("mouseenter", (evt) => {
+  console.log(evt.pageX, evt.pageY); // how far down scrolled
+});
+
+scrollable.addEventListener("mousemove", (evt) => {
+  console.log(evt.pageX, evt.pageY); // how far down scrolled
+}); */
+
+/* const btn = document.querySelector("button");
+const scrollable = document.querySelector("#scrollable");
+btn.addEventListener("dragstart", (evt) => {
+  console.log(evt);
+});
+
+scrollable.addEventListener("drop", (e) => {
+  scrollable.prepend(btn);
+});
+
+scrollable.addEventListener("dragover", (e) => {
+  e.preventDefault(); //allow call of drop evt
+});
+
+const back = document.querySelector("#back");
+
+back.addEventListener("drop", (e) => {
+  back.prepend(btn);
+});
+
+back.addEventListener("dragover", (e) => {
+  e.preventDefault(); //allow call of drop evt
+}); */
+
+/* document.body.addEventListener('drop', e=>{
+  document.body.append(btn);
+})
+
+document.body.addEventListener('drogover', e=>{
+  e.preventDefault();
+}) */
+
+//event delegation
+const btn = document.querySelector("button");
+const scrollable = document.querySelector("#scrollable");
+
+//evt to run on container
+//all element of container have same evt listener
+
+scrollable.addEventListener("click", (evt) => {
+  if (evt.target !== this) {
+    //click is not on the container
+    evt.target.textContent = "Clicked";
+  }
+});
