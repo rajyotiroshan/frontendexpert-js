f68e3550a4c66aba13582a0d31ad04f0d8f5f176
rajan <rajyotiroshan@gmail.com>
2023-01-16 20:59:58 +0530

    debugger;

--- _26_debugging_strat/index.js
+++ _26_debugging_strat/index.js
@@ -3,10 +3,19 @@ const btn = document.querySelector("button");
 //console.log(btn);
 btn.addEventListener("click", moveRight);
 
-function moveRight(evt) {
+/* function moveRight(evt) {
   //console.log("clicked");
   const circle = document.querySelector("#circle");
   const { left } = getComputedStyle(circle);
   //console.log(parseInt(left));
   circle.style.left = parseInt(left) + 10 + "px";
+} */
+
+//debug without console.log
+function moveRight(evt) {
+  //debugger;  //open js debugger with src code in chrom devtools.
+  const circle = document.querySelector("#circle");
+  const { left } = getComputedStyle(circle);
+
+  circle.style.left = left + 10;
 }
