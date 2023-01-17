a3510b3d1170f60694570fdd46aa73d65c318cd4
rajan <rajyotiroshan@gmail.com>
2023-01-17 18:33:35 +0530

    js writing clean code

---
+++ _27_clean_js.js
@@ -0,0 +1,62 @@
+//clean javascript
+//easy understand, update. maintain
+//idiomatic code: -> follows std practices of the lang
+/
+/**
+ * forEach(std func) => forEach(arr func)
+ */
+
+/**
+ * "" + "" => using template literal 
+ */
+
+/**
+ * Destructuring
+ * [,,] = arr
+ */
+
+/**
+ * overlly nesting cb func => async await.
+ */
+
+
+//overuse this key //no
+
+//donot bind to this if not making sense.
+
+//functional programming as much as possible
+/**
+ * functionaing chaining
+ * arr.filter().map().reverse()
+ */
+
+//Code autoformatter
+//plugin in editor
+//prettier
+//aumaticaaly indent,lot clear
+//consistency
+//same styleed code
+
+//use a syle guide, not speficic to js
+//goal to achieve consistency
+//small project -> google js style guide.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
